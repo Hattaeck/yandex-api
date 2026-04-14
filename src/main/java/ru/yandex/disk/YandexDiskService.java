52dcf427d823823cb0bc7f8df6144f2336c562d2
Aleksey <99037915+Hattaeck@users.noreply.github.com>
2026-04-14 16:25:08 +0300

    Added more tests

--- src/main/java/ru/yandex/disk/YandexDiskService.java
+++ src/main/java/ru/yandex/disk/YandexDiskService.java
@@ -7,6 +7,9 @@ public interface YandexDiskService {
     @GET("v1/disk/")
     Call<Void> getDiskInfo(@Header("Authorization") String token);
 
+    @GET("v1/disk/resources")
+    Call<Void> getResourceInfo(@Header("Authorization") String token, @Query("path") String path);
+
     @PUT("v1/disk/resources")
     Call<Void> createFolder(@Header("Authorization") String token, @Query("path") String path);
 
@@ -17,6 +20,16 @@ public interface YandexDiskService {
             @Query("path") String to
     );
 
+    @POST("v1/disk/resources/move")
+    Call<Void> moveResource(
+            @Header("Authorization") String token,
+            @Query("from") String from,
+            @Query("path") String to
+    );
+
     @DELETE("v1/disk/resources")
     Call<Void> deleteResource(@Header("Authorization") String token, @Query("path") String path);
+
+    @DELETE("v1/disk/trash/resources")
+    Call<Void> clearTrash(@Header("Authorization") String token);
 }
--- src/test/java/ru/yandex/disk/DiskRetrofitTest.java
+++ src/test/java/ru/yandex/disk/DiskRetrofitTest.java
@@ -41,7 +41,9 @@ public class DiskRetrofitTest {
     @DisplayName("PUT: Создание папки")
     public void testCreateFolder() throws IOException {
         String folderPath = "TestFolder_" + System.currentTimeMillis();
+
         Response<Void> response = service.createFolder(myToken, folderPath).execute();
+
         Assertions.assertEquals(201, response.code());
     }
 
@@ -52,6 +54,7 @@ public class DiskRetrofitTest {
         service.createFolder(myToken, from).execute();
 
         String to = "CopiedFolder_" + System.currentTimeMillis();
+
         Response<Void> response = service.copyResource(myToken, from, to).execute();
 
         int code = response.code();
@@ -66,6 +69,78 @@ public class DiskRetrofitTest {
         service.createFolder(myToken, path).execute();
 
         Response<Void> response = service.deleteResource(myToken, path).execute();
+
         Assertions.assertEquals(204, response.code());
+    }
+
+    @Test
+    @DisplayName("GET: Получение инфо о созданном ресурсе")
+    public void testGetResourceInfo() throws IOException {
+        String path = "InfoFolder_" + System.currentTimeMillis();
+        service.createFolder(myToken, path).execute();
+
+        Response<Void> response = service.getResourceInfo(myToken, path).execute();
+
+        Assertions.assertEquals(200, response.code());
+
+        service.deleteResource(myToken, path).execute();
+    }
+
+    @Test
+    @DisplayName("POST: Перемещение/Переименование ресурса")
+    public void testMoveFolder() throws IOException {
+        String from = "OldFolder_" + System.currentTimeMillis();
+        service.createFolder(myToken, from).execute();
+
+        String to = "RenamedFolder_" + System.currentTimeMillis();
+
+        Response<Void> response = service.moveResource(myToken, from, to).execute();
+
+        Assertions.assertEquals(201, response.code());
+    }
+
+    @Test
+    @DisplayName("DELETE: Ошибка 404 при удалении несуществующего ресурса")
+    public void testDeleteNonExistentFolder() throws IOException {
+        String path = "NonExistent_" + System.currentTimeMillis();
+
+        Response<Void> response = service.deleteResource(myToken, path).execute();
+
+        Assertions.assertEquals(404, response.code());
+    }
+
+    @Test
+    @DisplayName("PUT: Ошибка 401 при неверном токене")
+    public void testCreateFolderWithWrongToken() throws IOException {
+        String badToken = "OAuth 12345";
+
+        Response<Void> response = service.createFolder(badToken, "AnyPath").execute();
+
+        Assertions.assertEquals(401, response.code());
+    }
+
+    @Test
+    @DisplayName("PUT: Ошибка 409 при создании уже существующей папки")
+    public void testCreateDuplicateFolder() throws IOException {
+        String path = "Duplicate_" + System.currentTimeMillis();
+        service.createFolder(myToken, path).execute();
+
+        Response<Void> response = service.createFolder(myToken, path).execute();
+
+        Assertions.assertEquals(409, response.code());
+    }
+
+    @Test
+    @DisplayName("DELETE: Полная очистка корзины")
+    public void testClearTrash() throws IOException {
+        String path = "ForTrash_" + System.currentTimeMillis();
+        service.createFolder(myToken, path).execute();
+        service.deleteResource(myToken, path).execute();
+
+        Response<Void> response = service.clearTrash(myToken).execute();
+        int code = response.code();
+
+        Assertions.assertTrue(code == 204 || code == 202,
+                "Ожидался код 204 или 202, но пришел: " + code);
     }
 }
